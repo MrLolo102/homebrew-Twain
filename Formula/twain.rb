04184fa48855debad04149532b853164e59ec7bb
admin <tranhailong1998@gmail.com>
2020-09-24 17:49:25 +0700

    a

--- Formula/twain.rb
+++ Formula/twain.rb
@@ -5,7 +5,7 @@ class Twain < Formula
   desc "Cross-platform Text Expander written in Rust"
   homepage "https://github.com/MrLolo102/twain"
   url "https://github.com/MrLolo102/Twain/files/5275101/twain-mac.tar.gz"
-  sha256 "d0e2331379f5fd5a8b87d9d8d971f1c7fe6f13589bf646a43882eed968159932"
+  sha256 "f5d6b806c0db77a9acfc99a50ad7246125ea4b6364ccf8328d06d6e6467c7209"
   version "1.0"
 
   def install
