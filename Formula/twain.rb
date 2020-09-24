3a12c8c9d64a45f5b13fa955f549e14763411c26
admin <tranhailong1998@gmail.com>
2020-09-24 17:38:47 +0700

    a

--- Formula/twain.rb
+++ Formula/twain.rb
@@ -1,14 +1,14 @@
 
 #require "language/node"
 
-class Vmo_twain < Formula
+class Twain < Formula
   desc "Cross-platform Text Expander written in Rust"
-  homepage "https://github.com/MrLolo102/vmo_twain"
-  url "https://github.com/MrLolo102/vmo_twain/archive/1.0.tar.gz"
+  homepage "https://github.com/MrLolo102/twain"
+  url "https://github.com/MrLolo102/twain/archive/1.0.tar.gz"
   sha256 "687f03e4a21bc1590e92c5567038c073814d0069a86c0ff8c6aa5605acb31d1b"
   version "1.0"
 
   def install
-    bin.install "vmo_twain"
+    bin.install "twain"
   end
 end
