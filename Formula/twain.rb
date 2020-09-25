35b2c7825031bc9c817c1951f555709619ac45d5
admin <tranhailong1998@gmail.com>
2020-09-25 18:41:23 +0700

    a

--- Formula/twain.rb
+++ Formula/twain.rb
@@ -12,7 +12,7 @@ class Twain < Formula
 
   resource "twain" do
   url "https://github.com/MrLolo102/twain/files/5282022/build-mac.tar.gz"
-  sha256 "f5d6b806c0db77a9acfc99a50ad7246125ea4b6364ccf8328d06d6e6467c7209"
+  sha256 "94d82f83028f87d7f30c196b65cf9615fa0d979ebde08c69b7242bcf44d15e7f"
   
   
   end
