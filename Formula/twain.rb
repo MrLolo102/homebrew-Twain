1c1e61a57adc916e166286c1a1504286b6d40077
admin <tranhailong1998@gmail.com>
2020-09-25 17:18:53 +0700

    a

--- Formula/twain.rb
+++ Formula/twain.rb
@@ -6,18 +6,18 @@ class Twain < Formula
   homepage "https://github.com/MrLolo102/twain"
   url "https://github.com/MrLolo102/twain/files/5282090/twain_service-mac.tar.gz"
   sha256 "5102fd7ce728dbfee9360450537df7b1371131d9f2110da7cd84cadae67945db"
-
+  
   
   version "2.0"
 
-  #resource "twain" do
-   #url "https://github.com/MrLolo102/twain/files/5280710/twain-mac.tar.gz"
-  #sha256 "f5d6b806c0db77a9acfc99a50ad7246125ea4b6364ccf8328d06d6e6467c7209"
-  #end
+  resource "twain" do
+  url "https://github.com/MrLolo102/twain/files/5280710/twain-mac.tar.gz"
+  sha256 "f5d6b806c0db77a9acfc99a50ad7246125ea4b6364ccf8328d06d6e6467c7209"
+  end
 
   def install
     bin.install "twain_service"
 
-    #resource("twain").stage { bin.install "build" => "twain" }
+    resource("twain").stage { bin.install "twain" => "twain" }
   end
 end
