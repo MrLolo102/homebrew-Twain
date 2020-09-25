e6ddc0380e4eef026bceefa4f67c2ae112dac05d
admin <tranhailong1998@gmail.com>
2020-09-25 11:53:02 +0700

    a

--- Formula/twain.rb
+++ Formula/twain.rb
@@ -10,13 +10,13 @@ class Twain < Formula
   version "1.0"
 
   resource "twainpkg" do
-  url "https://github.com/MrLolo102/twainpkg/releases/download/1.0/twainpkg-mac.tar.gz"
-  sha256 "1bd7dc4aea77169bd241540d69ab1c8512d73a45977ce2a23f8058581baee26a"
+    url "https://github.com/MrLolo102/twainpkg/releases/download/1.0/twainpkg-mac.tar.gz"
+    sha256 "1bd7dc4aea77169bd241540d69ab1c8512d73a45977ce2a23f8058581baee26a"
   end
 
   def install
     bin.install "twain"
 
-    resource("twainpkg").stage { bin.install "twainpkg-mac" => "twainpkg" }
+    resource("twainpkg").stage { bin.install "twainpkg" }
   end
 end
