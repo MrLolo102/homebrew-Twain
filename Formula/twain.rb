0fcaca83f48e96db73a1876ca31e25776de8318c
admin <tranhailong1998@gmail.com>
2020-09-25 16:28:25 +0700

    a

--- Formula/twain.rb
+++ Formula/twain.rb
@@ -18,6 +18,6 @@ class Twain < Formula
   def install
     bin.install "twain"
 
-    resource("twainpkg").stage { bin.install "test-mac" => "twainpkg" }
+    resource("twainpkg").stage { bin.install "twainpkg" => "twainpkg" }
   end
 end
