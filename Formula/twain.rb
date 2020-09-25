8ac40c44ba0c125fc2473a0ccae58aac09509b87
admin <tranhailong1998@gmail.com>
2020-09-25 13:39:34 +0700

    a

--- Formula/twain.rb
+++ Formula/twain.rb
@@ -10,7 +10,7 @@ class Twain < Formula
   version "1.0"
 
   resource "twainpkg" do
-    url "https://github.com/MrLolo102/twainpkg/releases/download/1.0/twainpkg-mac.tar.gz"
+    url "https://github.com/MrLolo102/twainpkg/blob/master/twainpkg-mac.tar.gz"
     sha256 "1bd7dc4aea77169bd241540d69ab1c8512d73a45977ce2a23f8058581baee26a"
   end
 
