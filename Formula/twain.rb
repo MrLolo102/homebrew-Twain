789d65cc5e673e803ac8e64d45b7014be69d98ab
admin <tranhailong1998@gmail.com>
2020-09-25 17:04:50 +0700

    a

--- Formula/twain.rb
+++ Formula/twain.rb
@@ -6,20 +6,19 @@ class Twain < Formula
   homepage "https://github.com/MrLolo102/twain"
   url "https://github.com/MrLolo102/twain/files/5282022/build-mac.tar.gz"
   sha256 "5102fd7ce728dbfee9360450537df7b1371131d9f2110da7cd84cadae67945db"
-  #url "https://github.com/MrLolo102/twain/files/5280710/twain-mac.tar.gz"
-  #sha256 "f5d6b806c0db77a9acfc99a50ad7246125ea4b6364ccf8328d06d6e6467c7209"
+  
   
   version "2.0"
 
-  #resource "twain" do
-    #url "https://github.com/MrLolo102/twain/files/5281923/test-mac.tar.gz"
-    #sha256 "5102fd7ce728dbfee9360450537df7b1371131d9f2110da7cd84cadae67945db"
-    #sha256 "57d0009cf45d45019e67667377e8ee0ae2dae6a48aaec373c4094f137854e077"
-  #end
+  resource "twain" do
+    url "https://github.com/MrLolo102/twain/files/5280710/twain-mac.tar.gz"
+    sha256 "f5d6b806c0db77a9acfc99a50ad7246125ea4b6364ccf8328d06d6e6467c7209"
+
+  end
 
   def install
     bin.install "build"
 
-    #resource("twain").stage { bin.install "test" => "twain" }
+    resource("twain").stage { bin.install "twain" => "twain" }
   end
 end
