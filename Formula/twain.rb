b7d33e2dcd85a11f7da21e41ac50bf5360e176a4
admin <tranhailong1998@gmail.com>
2020-09-28 15:01:13 +0700

    a

--- Formula/twain.rb
+++ Formula/twain.rb
@@ -2,34 +2,26 @@
 #require "language/node"
 
 class Twain < Formula
-  #desc "Cross-platform Text Expander written in Rust"
-  #homepage "https://github.com/MrLolo102/twain"
-  #url "https://github.com/MrLolo102/twain/files/5280710/twain-mac.tar.gz"
-  #sha256 "f5d6b806c0db77a9acfc99a50ad7246125ea4b6364ccf8328d06d6e6467c7209"
-  
- 
-  #version "2.0"
-
-  #resource "twain" do
-  #url "https://github.com/MrLolo102/twain/tree/master/build"
-  #sha256 "2a2787bf2f24ae710754cceb8e52ed6dfcaf296b88789e179b14b8c1affaa96b"
-  
-  
-  #end
-
-  #def install
-    #bin.install "twain"
-   
-  #end
   desc "Cross-platform Text Expander written in Rust"
   homepage "https://github.com/MrLolo102/twain"
+  url "https://github.com/MrLolo102/twain/files/5280710/twain-mac.tar.gz"
+  sha256 "f5d6b806c0db77a9acfc99a50ad7246125ea4b6364ccf8328d06d6e6467c7209"
+  
+ 
+  version "2.0"
+
+  resource "twain" do
   url "https://github.com/MrLolo102/twain/releases/download/2.0/twain-mac.tar.gz"
   sha256 "f019964241186a51c2fd23b5421847dc80a99ab06a3f4da7a66a111ba17d5b43"
-  #head "https://github.com/silnrsi/graphite.git"
-
+  end
+ 
   def install
-    system "./configure", "--prefix=#{prefix}"
-    system "make", "install"
+   bin.install "twain"
+   
+   resource("twain").stage {
+   	system "./configure", "--prefix=#{prefix}"
+   	system "make", "install"}
+   
   end
 end
 
