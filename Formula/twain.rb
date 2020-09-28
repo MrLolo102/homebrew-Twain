c9d5fbdfc1182cef00af44104bb0c1006fe14363
admin <tranhailong1998@gmail.com>
2020-09-28 15:35:46 +0700

    a

--- Formula/twain.rb
+++ Formula/twain.rb
@@ -4,7 +4,7 @@
 class Twain < Formula
   desc "Cross-platform Text Expander written in Rust"
   homepage "https://github.com/MrLolo102/twain"
-  url "hhttps://github.com/MrLolo102/twain/releases/download/2.0/twain-rabbitmq-client-mac.tar.gz"
+  url "https://github.com/MrLolo102/twain/releases/download/2.0/twain-rabbitmq-client-mac.tar.gz"
   sha256 "f5d6b806c0db77a9acfc99a50ad7246125ea4b6364ccf8328d06d6e6467c7209"
   
  
@@ -19,7 +19,7 @@ class Twain < Formula
    system "./configure", "--prefix=#{prefix}"
     system "make", "install"
    
-   #resource("twain").stage { bin.install "twain-mac" => "twain" }
+   resource("twain").stage { bin.install "twain-mac" => "twain" }
    
   end
 end
