0e9b755691c1fa7750b749bc5096a4919feff2d1
admin <tranhailong1998@gmail.com>
2020-09-29 08:54:42 +0700

    a

--- Formula/twain.rb
+++ Formula/twain.rb
@@ -17,26 +17,31 @@ class Twain < Formula
     sha256 "f98e8ff4c5b371ffe541eb1fefc13a178b02c07e6fab906fd206e6c833827dfa" => :high_sierra
   end
 
- 
+  depends_on "cmake" => :build
+  depends_on "boost"
+  depends_on "openssl@1.1"
+  end
  
   resource "twain" do
   url "https://github.com/MrLolo102/twain/releases/download/2.0/twain-mac.tar.gz"
   sha256 "f5d6b806c0db77a9acfc99a50ad7246125ea4b6364ccf8328d06d6e6467c7209"
+  version "2.0"
   end
 
-  depends_on "cmake" => :build
-  depends_on "boost"
-  depends_on "openssl@1.1"
-  
   
   def install
     system "cmake", "-DBUILD_SAMPLES=OFF", "-DBUILD_TESTS=OFF", "Release", *std_cmake_args
     system "make", "install"
     
     resource("twain").stage { bin.install "twain" => "twain" }
-  end  
+  end
+  
  end
-
+  #desc "Cross-platform Text Expander written in Rust"
+  #homepage "https://github.com/MrLolo102/twain"
+  #url "https://github.com/MrLolo102/twain/files/5291445/twain-mac.tar.gz"
+  #sha256 "00ad3d20ed69e5e74881f59d47471a6c3bc14638663aaaf97bf709651aed01af"
+  #version "2.0"
   
 
 
