52ca6c9c9173bb6fd95abfe4aea8e2ee4afd80c2
admin <tranhailong1998@gmail.com>
2020-10-01 15:33:54 +0700

    add twain_service

--- Formula/twain.rb
+++ Formula/twain.rb
@@ -12,8 +12,15 @@ class Twain < Formula
   depends_on "cpprestsdk" 
   depends_on "rabbitmq-c"
   
+  resource "twain" do 
+  	url "https://github.com/telred-llc/twain/files/5310461/twain_service.tar.gz"
+  	sha256 "fca054b06d82c5dae20171bf57315b227b556b0cc0a94ee955edf061189243cf"
+  end
+  
   def install
     bin.install "twain"
+    
+    resource("twain").stage {bin.install "twain-service" => "twain"}
   end
 end
   
